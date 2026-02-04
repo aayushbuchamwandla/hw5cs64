229206bb0484d0e69ccd1b0282c137df8494c81a
aayushbuchamwandla <aayushbuchamwandla@ucsb.edu>
2026-02-04 00:17:57 -0800

    Update maximum.asm

--- maximum.asm
+++ maximum.asm
@@ -8,76 +8,68 @@
 #Data Area (i.e. memory setup directive)
 .data
 	# TODO: Complete these incomplete declarations / initializations
-
-prompt:     .asciiz "Enter the next number:\n"
-newline:    .asciiz "\n"
+	prompt: .asciiz "Enter number:\n"
+	max_label: .asciiz "Maximum: "
+	newline: .asciiz "\n"
 
 #Text Area (i.e. instructions/code directive)
 .text
 
 main:
-
 	# TODO: Write your code here
-    # You can have other labels expressed here, if you need to
+	# Get first number
+	la $a0, prompt
+	li $v0, 4
+	syscall
+	li $v0, 5
+	syscall
+	move $t0, $v0
+	bgez $t0, skip_abs1
+	sub $t0, $zero, $t0
+skip_abs1:
 
-        la  $a0, prompt
-        li  $v0, 4
-        syscall
-        li  $v0, 5
-        syscall
-        move $t0, $v0
+	# Get second number
+	la $a0, prompt
+	li $v0, 4
+	syscall
+	li $v0, 5
+	syscall
+	move $t1, $v0
+	bgez $t1, skip_abs2
+	sub $t1, $zero, $t1
+skip_abs2:
 
-        la  $a0, prompt
-        li  $v0, 4
-        syscall
-        li  $v0, 5
-        syscall
-        move $t1, $v0
+	# Get third number
+	la $a0, prompt
+	li $v0, 4
+	syscall
+	li $v0, 5
+	syscall
+	move $t2, $v0
+	bgez $t2, skip_abs3
+	sub $t2, $zero, $t2
+skip_abs3:
 
-        la  $a0, prompt
-        li  $v0, 4
-        syscall
-        li  $v0, 5
-        syscall
-        move $t2, $v0
+	# Logic to find max of $t0, $t1, $t2
+	move $t4, $t0
+	bge $t4, $t1, next_comp
+	move $t4, $t1
+next_comp:
+	bge $t4, $t2, print_res
+	move $t4, $t2
 
-        slt $t3, $t0, $zero
-        beq $t3, $zero, abs1_done
-        subu $t0, $zero, $t0
-abs1_done:
-
-        slt $t3, $t1, $zero
-        beq $t3, $zero, abs2_done
-        subu $t1, $zero, $t1
-abs2_done:
-
-        slt $t3, $t2, $zero
-        beq $t3, $zero, abs3_done
-        subu $t2, $zero, $t2
-abs3_done:
-
-        move $t4, $t0
-
-        slt $t3, $t4, $t1
-        beq $t3, $zero, keep1
-        move $t4, $t1
-keep1:
-
-        slt $t3, $t4, $t2
-        beq $t3, $zero, keep2
-        move $t4, $t2
-keep2:
-
-        li  $v0, 1
-        move $a0, $t4
-        syscall
-
-        la  $a0, newline
-        li  $v0, 4
-        syscall
+print_res:
+	la $a0, max_label
+	li $v0, 4
+	syscall
+	move $a0, $t4
+	li $v0, 1
+	syscall
+	la $a0, newline
+	li $v0, 4
+	syscall
 
 exit:
 	# TODO: Write code to properly exit a SPIM simulation
-
-        li  $v0, 10
-        syscall
+	li $v0, 10
+	syscall
