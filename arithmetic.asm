9ade614217e7caa5ab18e1715f91a43426010fed
aayushbuchamwandla <aayushbuchamwandla@ucsb.edu>
2026-02-04 00:21:15 -0800

    Update arithmetic.asm

--- arithmetic.asm
+++ arithmetic.asm
@@ -1,41 +1,29 @@
-# arithmetic.asm program
-# CS 64, Z.Matni
-
 .text
 main:
-    # Get a
     li $v0, 5
     syscall
-    move $t0, $v0 # $t0 = a
+    move $t0, $v0
 
-    # Get b
     li $v0, 5
     syscall
-    move $t1, $v0 # $t1 = b
+    move $t1, $v0
 
-    # Get c
     li $v0, 5
     syscall
-    move $t2, $v0 # $t2 = c
+    move $t2, $v0
 
-    # Calculate 5 * b using mult (the one allowed mult)
     li $t3, 5
     mult $t1, $t3
-    mflo $t4      # $t4 = 5b
+    mflo $t4
 
-    # Calculate (a - 5b)
-    sub $t5, $t0, $t4 # $t5 = a - 5b
+    sub $t5, $t0, $t4
 
-    # Calculate 8 * (a - 5b) using shift
-    sll $t6, $t5, 3   # $t6 = (a - 5b) * 2^3
+    sll $t6, $t5, 3
 
-    # Calculate 64 * c using shift
-    sll $t7, $t2, 6   # $t7 = c * 2^6
+    sll $t7, $t2, 6
 
-    # Final result
     add $a0, $t6, $t7
 
-    # Print result
     li $v0, 1
     syscall
 
