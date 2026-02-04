02d81234675ff8b9e4094ab85b289322fc5fa381
aayushbuchamwandla <aayushbuchamwandla@ucsb.edu>
2026-02-04 00:21:45 -0800

    Update fibonacci.asm

--- fibonacci.asm
+++ fibonacci.asm
@@ -1,96 +1,65 @@
-# fibonacci.asm program
-# CMPSC 64, (c) 2025, Z.Matni
-#
-#    unsigned int n, f1 = 1, f2 = 1, i = 2, nextfib;
-#    cout << "Enter a number:\n";
-#    cin >> n;
-#    if (n > 1) {
-#        cout << "The first " << n << " numbers in the Fibonacci series are:\n";
-#        cout << f1 << " " << f2;
-#        while (i < n) {
-#            nextfib = f1 + f2;
-#            f1 = f2;
-#            f2 = nextfib;
-#            cout << " " << nextfib;
-#            i++;
-#        }
-#    }
-#   cout << endl;
-
 .data
-	prompt:     .asciiz "Enter a number:\n"
+    prompt:     .asciiz "Enter a number:\n"
     first:      .asciiz "The first "
     numbers:    .asciiz " numbers in the Fibonacci series are:\n"
-	spacech:    .asciiz " "
+    spacech:    .asciiz " "
     newline:    .asciiz "\n"
 	
 .text
 main:
-# TODO: Follow the comments below, and the assignment description, 
-#        as guides for how to write this assembly program.
-
-# Print the prompt and get user input
-	la $a0, prompt
-	li $v0, 4
-	syscall
-	li $v0, 5
-	syscall
-	move $t0, $v0 # $t0 is n
+    la $a0, prompt
+    li $v0, 4
+    syscall
+    li $v0, 5
+    syscall
+    move $t0, $v0 
 	
-# Initialize all registers needed in the program (as appropriate)
-	li $t1, 1 # f1
-	li $t2, 1 # f2
-	li $t3, 2 # i
+    li $t1, 1 
+    li $t2, 1 
+    li $t3, 2 
 
-# if (exit conditions are met) then go to exit
-	ble $t0, 1, exit
+    ble $t0, 1, exit
 
-# otherwise:
-    # print "The first " n " numbers in the Fibonacci series are:\n"
-	la $a0, first
-	li $v0, 4
-	syscall
-	move $a0, $t0
-	li $v0, 1
-	syscall
-	la $a0, numbers
-	li $v0, 4
-	syscall
+    la $a0, first
+    li $v0, 4
+    syscall
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    la $a0, numbers
+    li $v0, 4
+    syscall
 
-    # print f1 " " f2
-	li $a0, 1
-	li $v0, 1
-	syscall
-	la $a0, spacech
-	li $v0, 4
-	syscall
-	li $a0, 1
-	li $v0, 1
-	syscall
+    li $a0, 1
+    li $v0, 1
+    syscall
+    la $a0, spacech
+    li $v0, 4
+    syscall
+    li $a0, 1
+    li $v0, 1
+    syscall
 
-    # while loop:
 loop:
-	bge $t3, $t0, exit
-	add $t4, $t1, $t2 # nextfib
-	move $t1, $t2     # f1 = f2
-	move $t2, $t4     # f2 = nextfib
+    bge $t3, $t0, exit
+    add $t4, $t1, $t2 
+    move $t1, $t2     
+    move $t2, $t4     
 	
-	la $a0, spacech
-	li $v0, 4
-	syscall
-	move $a0, $t4
-	li $v0, 1
-	syscall
+    la $a0, spacech
+    li $v0, 4
+    syscall
+    move $a0, $t4
+    li $v0, 1
+    syscall
 	
-	addi $t3, $t3, 1
-	j loop
+    addi $t3, $t3, 1
+    j loop
 
 exit:
-# print newline
-	la $a0, newline
-	li $v0, 4
-	syscall
+    la $a0, newline
+    li $v0, 4
+    syscall
 
-# quit program
-	li $v0, 10
-	syscall
+    li $v0, 10
+    syscall
