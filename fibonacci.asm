2fd461b55b5d467d22b0105df04de1a6c78a3426
aayushbuchamwandla <aayushbuchamwandla@ucsb.edu>
2026-02-03 23:58:09 -0800

    Update fibonacci.asm

--- fibonacci.asm
+++ fibonacci.asm
@@ -1,75 +1,113 @@
-.data
-prompt:     .asciiz "Enter a number:\n"
-first:      .asciiz "The first "
-numbers:    .asciiz " numbers in the Fibonacci series are:\n"
-spacech:    .asciiz " "
-newline:    .asciiz "\n"
+# fibonacci.asm program
+# CMPSC 64, (c) 2025, Z.Matni
+#
+#    unsigned int n, f1 = 1, f2 = 1, i = 2, nextfib;
+#    cout << "Enter a number:\n";
+#    cin >> n;
+#    if (n > 1) {
+#        cout << "The first " << n << " numbers in the Fibonacci series are:\n";
+#        cout << f1 << " " << f2;
+#        while (i < n) {
+#            nextfib = f1 + f2;
+#            f1 = f2;
+#            f2 = nextfib;
+#            cout << " " << nextfib;
+#            i++;
+#        }
+#    }
+#   cout << endl;
 
+.data
+	prompt:     .asciiz "Enter a number:\n"
+    first:      .asciiz "The first "
+    numbers:    .asciiz " numbers in the Fibonacci series are:\n"
+	spacech:    .asciiz " "
+    newline:    .asciiz "\n"
+	
 .text
 main:
-	la  $a0, prompt
-	li  $v0, 4
-	syscall
+# TODO: Follow the comments below, and the assignment description, 
+#       as guides for how to write this assembly program.
 
-	li  $v0, 5
-	syscall
-	move $t0, $v0
+        la  $a0, prompt
+        li  $v0, 4
+        syscall
 
-	li  $t5, 1
-	slt $t6, $t5, $t0
-	beq $t6, $zero, exit
+        li  $v0, 5
+        syscall
+        move $t0, $v0
 
-	li  $t1, 1
-	li  $t2, 1
-	li  $t3, 2
+# Print the prompt and get user input
+	
+# Initialize all registers needed in the program (as appropriate)
 
-	la  $a0, first
-	li  $v0, 4
-	syscall
+        li  $t1, 1
+        li  $t2, 1
+        li  $t3, 2
 
-	li  $v0, 1
-	move $a0, $t0
-	syscall
+# if (exit conditions are met) then go to exit
+# otherwise:
 
-	la  $a0, numbers
-	li  $v0, 4
-	syscall
+        li  $t4, 1
+        slt $t5, $t4, $t0
+        beq $t5, $zero, exit
 
-	li  $v0, 1
-	move $a0, $t1
-	syscall
+    # print "The first " n " numbers in the Fibonacci series are:\n"
+    # print f1 " " f2
 
-	la  $a0, spacech
-	li  $v0, 4
-	syscall
+        la  $a0, first
+        li  $v0, 4
+        syscall
 
-	li  $v0, 1
-	move $a0, $t2
-	syscall
+        li  $v0, 1
+        move $a0, $t0
+        syscall
+
+        la  $a0, numbers
+        li  $v0, 4
+        syscall
+
+        li  $v0, 1
+        move $a0, $t1
+        syscall
+
+        la  $a0, spacech
+        li  $v0, 4
+        syscall
+
+        li  $v0, 1
+        move $a0, $t2
+        syscall
+
+    # while loop:
 
 fib_loop:
-	sltu $t6, $t3, $t0
-	beq  $t6, $zero, exit
+        sltu $t5, $t3, $t0
+        beq  $t5, $zero, exit
 
-	addu $t4, $t1, $t2
-	move $t1, $t2
-	move $t2, $t4
+        addu $t6, $t1, $t2
+        move $t1, $t2
+        move $t2, $t6
 
-	la  $a0, spacech
-	li  $v0, 4
-	syscall
+        la  $a0, spacech
+        li  $v0, 4
+        syscall
 
-	li  $v0, 1
-	move $a0, $t4
-	syscall
+        li  $v0, 1
+        move $a0, $t6
+        syscall
 
-	addiu $t3, $t3, 1
-	j fib_loop
+        addiu $t3, $t3, 1
+        j fib_loop
 
 exit:
-	la  $a0, newline
-	li  $v0, 4
-	syscall
+# print newline
 
-	li  $v0, 10
-	syscall
+        la  $a0, newline
+        li  $v0, 4
+        syscall
+
+# quit program
+
+        li  $v0, 10
+        syscall
